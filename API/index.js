1a2704684e06c6df8be4de7b9894a211a63c5bd7
YashXBansal <yashbansal414@gmail.com>
2023-11-18 21:01:04 +0530

    create listing API route

--- API/index.js
+++ API/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
+import { createListing } from './controllers/listing.controller.js';
 import cookieParser from 'cookie-parser';
 dotenv.config();
 
@@ -30,6 +31,7 @@ app.listen(PORT, () => {
 
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
+app.use("/api/listing", createListing);
 
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
